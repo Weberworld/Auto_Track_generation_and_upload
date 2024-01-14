4522d94d29b4fd3a141e9342762b56ead4f9a335
Weberworld <omotosoboluwatife23@gmail.com>
2024-01-14 01:35:12 +0100

    fixing

--- soundcloud_uploads/soundcloud.py
+++ soundcloud_uploads/soundcloud.py
@@ -57,20 +57,20 @@ class SoundCloud:
             print(self.driver.current_url)
             if secs_waited_for < Settings.TIMEOUT:
                 time.sleep(1)
-
-                try:
-                    # Send the verification code to the user email
-                    print("Sending message")
-                    send_telegram_message("Trying to retrive auth code")
-                    code = self.driver.get_text("div.VBGMK > span", timeout=Settings.TIMEOUT)
-                    print("Verification code has been sent to the user's phone. Please confirm registration")
-                    print(f"The Verification code is {code}")
-                    send_telegram_message(f"Upwork Bot requests to login your google account.\n"
-                                          f"The Verification code is {code}")
-                    self.driver.sleep(Settings.TIMEOUT)
-                except (TimeoutException, NoSuchElementException):
-                    return self.login(link, username, password)
             secs_waited_for += 1
+            try:
+                # Send the verification code to the user email
+                print("Sending message")
+                send_telegram_message("Trying to retrive auth code")
+                code = self.driver.get_text("div.VBGMK > span", timeout=Settings.TIMEOUT)
+                print("Verification code has been sent to the user's phone. Please confirm registration")
+                print(f"The Verification code is {code}")
+                send_telegram_message(f"Upwork Bot requests to login your google account.\n"
+                                      f"The Verification code is {code}")
+                self.driver.sleep(Settings.TIMEOUT)
+            except (TimeoutException, NoSuchElementException):
+                return self.login(link, username, password)
+
         self.driver.sleep(2)
         print("Login success")
 
