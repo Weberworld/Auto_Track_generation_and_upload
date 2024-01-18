938b85a7addbecb5ac2d0dfda8d2b0fac8075482
Weberworld <omotosoboluwatife23@gmail.com>
2024-01-18 02:16:06 +0100

    test deploy

--- main2.py
+++ main2.py
@@ -19,7 +19,7 @@ def wait_randomly():
     time.sleep(random.randint(1, 5))
 
 
-@sched.scheduled_job('cron', day_of_week='mon-sun', hour=1, minute=30)
+@sched.scheduled_job('cron', day_of_week='mon-sun', hour=1, minute=20)
 def automation_process():
     # Connect to the redis server
     r = redis.from_url(os.environ.get("REDISCLOUD_URL"))
